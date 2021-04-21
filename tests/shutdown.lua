6001bb152d8c21a4b88e6da6be1b89cd88cdc35a
Sergey Nikiforov <void@tarantool.org>
2021-04-21 17:45:22 +0300

    more shutdown tests
    Part #6

--- tests/shutdown.lua
+++ tests/shutdown.lua
@@ -14,6 +14,13 @@ g.test_double_cfg = function()
     http.shutdown()
 end
 
+g.test_shutdown_after_wrong_cfg = function()
+    t.assert_error_msg_content_equals('No parameters specified',
+        http.cfg)
+    t.assert_error_msg_content_equals('Server is not launched',
+        http.shutdown)
+end
+
 g.test_unexpected_shutdown = function()
     t.assert_error_msg_content_equals('Server is not launched',
         http.shutdown)
